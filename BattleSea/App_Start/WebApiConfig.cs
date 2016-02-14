4b7bcb4ed8fd2f6f29d4fb6a95919c6f34941414
Veceslav Cartera <vcartera@gmail.com>
2016-02-14 13:30:03 +0200

    -imported Angular, set up Controller

--- BattleSea/App_Start/WebApiConfig.cs
+++ BattleSea/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,11 @@ namespace BattleSea
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
